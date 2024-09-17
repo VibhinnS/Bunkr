29940f5321580a9faea7a622fb4a0cd189534fe5
VibhinnS <vibhinnsinghal25@gmail.com>
2024-09-17 10:03:15 +0530

    Set up CORS for auth service

--- auth-system/authentication/authentication/settings.py
+++ auth-system/authentication/authentication/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'platformusers'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +125,26 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:5173'
+]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
+
+CORS_ALLOW_HEADERS = [
+    'content-type',
+    'authorization',
+    'x-csrftoken',
+    'x-requested-with',
+]
--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express, {Express, Request, Response} from "express"
 import cors from "cors";
+import fetch from "cross-fetch";
 
 let app: Express = express();
 app.use(express.json())
@@ -10,5 +11,39 @@ app.get('/', (req: Request, res: Response) => {
         msg: "App working properly"
     })
 })
+
+app.post('/signin', async (req: Request, res: Response) => {
+    try {
+        const response = await fetch('http://localhost:8000/api/sign-in', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body:JSON.stringify( {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+            }),
+        });
+
+        const data = await response.json(); 
+        console.log(data);
+        res.status(response.status).json(data);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000)

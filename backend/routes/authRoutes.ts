6be4058fd620b0a286ae1e8bd238cd7dbe52802e
VibhinnS <vibhinnsinghal25@gmail.com>
2024-09-20 12:12:05 +0530

    Added reqmnts file

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signin, signup, forgotPassword } from '../controllers/AuthController'
+import { signin, signup, forgotPassword, posts } from '../controllers/AuthController'
 import validatePayload from '../middlewares/validatePayload';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/signup', validatePayload('signup'), signup);
 router.post('/signin', validatePayload('signin'), signin);
 router.post('/forgot-password', validatePayload('forgotPassword'), forgotPassword);
+router.post('/posts', validatePayload('posts'), posts)
 
 export default router;
